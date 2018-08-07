bee5e4a491857ec73a39b0c9bcd547809d3ba4a3
imABEING <kamilahamitchell@gmail.com>
2018-08-07 01:48:05 -0400

    Add files via upload

--- traumaDemo.py
+++ traumaDemo.py
@@ -4,11 +4,25 @@
 
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
+import matplotlib.pyplot as plt
+
 
 #Import & Analyze Audio Signal
 [Fs, x] = audioBasicIO.readAudioFile("OSIRIS_Motif.wav");
-#F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
-F = audioFeatureExtraction.stFeatureExtraction(x[:,0], Fs, 0.050*Fs, 0.025*Fs)
+
+#Mono
+if (x.any()==1):
+    nuX = audioBasicIO.stereo2mono(x);
+    F = audioFeatureExtraction.stFeatureExtraction(nuX, Fs, 0.050*Fs, 0.025*Fs);
+
+#Stereo
+else:
+    F = audioFeatureExtraction.stFeatureExtraction(x[:,0], Fs, 0.050*Fs, 0.025*Fs)
+
+
+plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel('ZCR');
+plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel('Energy');
+plt.subplot(2,1,9); plt.plot(F[9,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC'); plt.show()
 
 
 
