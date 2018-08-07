e9ea7ed3dac1a8bf002e29d3d21b4931d21ea0cc
imABEING <kamilahamitchell@gmail.com>
2018-08-07 01:21:26 -0400

    Add files via upload

--- traumaDemo.py
+++ traumaDemo.py
@@ -1,39 +1,29 @@
 
 #!/usr/bin/python
-import wave
-import pyaudio
 
 
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
 
-#import os, glob, eyed3, ntpath, shutil
-
+#Import & Analyze Audio Signal
 [Fs, x] = audioBasicIO.readAudioFile("OSIRIS_Motif.wav");
-F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+#F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
+F = audioFeatureExtraction.stFeatureExtraction(x[:,0], Fs, 0.050*Fs, 0.025*Fs)
 
 
-chunk = 4096
 
-f = wave.open(r"/Users/kamilahmitchell/Desktop/C++, Python & Vsts/Neurolex/OSIRIS_Motif.wav","rb")
 
-p = pyaudio.PyAudio()
-#open stream
-stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-                channels = f.getnchannels(),
-                rate = f.getframerate(),
-                output = True)
-#read data
-data = f.readframes(chunk)
 
-#play stream
-while data:
-    stream.write(data)
-    data = f.readframes(chunk)
 
-#stop stream
-stream.stop_stream()
-#stream.close()
-
-#close PyAudio
-p.terminate()
+#Citations
+'''
+@article{giannakopoulos2015pyaudioanalysis,
+    title={pyAudioAnalysis: An Open-Source Python Library for Audio Signal Analysis},
+        author={Giannakopoulos, Theodoros},
+            journal={PloS one},
+                volume={10},
+                    number={12},
+                        year={2015},
+                            publisher={Public Library of Science}
+}
+'''
