7142534eb69b9d404fbe0a94f0280caec70823df
imABEING <kamilahamitchell@gmail.com>
2018-09-10 14:23:48 -0400

    returns formant frequencies, plots data

--- traumaDemo.py
+++ traumaDemo.py
@@ -10,72 +10,40 @@ from pyAudioAnalysis import audioFeatureExtraction
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as style
 
+def findFormants():
 
-#Import & Analyze Audio Signal
-nfft = 64
-X = 64
-nceps = 32
+    #Define Variables
+    nfft = 256
+    X = 32
+    nceps = 64
 
-[Fs, x] = audioBasicIO.readAudioFile("PTSD_female.wav");
+    #Import & Analyze Audio Signal
+    [Fs, x] = audioBasicIO.readAudioFile("PTSD_female2.wav");
 
+    #[HR, f0] = audioFeatureExtraction.stHarmonic(x[:,0], Fs)
 
-#Mono
-if (x.any==1):
-    nuX = audioBasicIO.stereo2mono(x);
-    F = audioFeatureExtraction.stFeatureExtraction(nuX, Fs, 0.050*Fs, 0.025*Fs);
-#Stereo
-else:
-    F = audioFeatureExtraction.stFeatureExtraction(x[:,0], Fs, 0.050*Fs, 0.025*Fs)
+    #Calculate Formant Frequencies
+    frqs = audioFeatureExtraction.phormants(x[:,0], Fs);
 
+    #Calculate MFCCs
+    [fbank, freqs] = audioFeatureExtraction.mfccInitFilterBanks(Fs, nfft);
+    ceps = audioFeatureExtraction.stMFCC(X, fbank, nceps);
 
-[fbank, freqs] = audioFeatureExtraction.mfccInitFilterBanks(Fs, nfft)
-
-
-ceps = audioFeatureExtraction.stMFCC(X, fbank, nceps);
-
-print ceps
-
-#chn = audiofile.channels
-
-'''
-#Mono
-if (x.any==1):
-    nuX = audioBasicIO.stereo2mono(x);
-    F = audioFeatureExtraction.stFeatureExtraction(nuX, Fs, 0.050*Fs, 0.025*Fs);
-#Stereo
-elif (x.any==2):
-    F = audioFeatureExtraction.stFeatureExtraction(x[:,0], Fs, 0.050*Fs, 0.025*Fs)
-
-#Else
-else:
-    print ('Please input a Stereo or Mono file')
-#return 1
-'''
+    return frqs
 
 
+#Call Functions
+frqs = findFormants();
+print frqs
+
+
+#Visualize Data
 plt.title('MFCC Analysis')
-plt.subplot(1,1,1); plt.plot(F[9,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC');
-plt.subplot(1,1,1); plt.plot(F[10,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC 2');
-plt.subplot(1,1,1); plt.plot(F[11,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC 3');
-plt.subplot(1,1,1); plt.plot(F[12,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC 4');
-plt.subplot(1,1,1); plt.plot(F[13,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC 5');
-plt.subplot(1,1,1); plt.plot(F[14,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC 6');
+#plt.subplot(1,1,1); plt.plot(F[9,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC');
+#plt.subplot(1,1,1); plt.plot(ceps[:,0]); plt.xlabel('Frame no'); plt.ylabel('Freq (Hz)');
+plt.subplot(1,1,1); plt.plot(frqs); plt.xlabel('Frame no'); plt.ylabel('Freq (Hz)');
 
 
 plt.show()
 
 
-
-
-#Citations
-'''
-@article{giannakopoulos2015pyaudioanalysis,
-    title={pyAudioAnalysis: An Open-Source Python Library for Audio Signal Analysis},
-        author={Giannakopoulos, Theodoros},
-            journal={PloS one},
-                volume={10},
-                    number={12},
-                        year={2015},
-                            publisher={Public Library of Science}
-}
-'''
