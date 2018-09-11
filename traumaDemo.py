f5eef3cf5a493fd215356515af872e4108c6762b
imABEING <kamilahamitchell@gmail.com>
2018-09-10 20:54:36 -0400

    Add files via upload
    Reads audio file array, finds formant values and plots data to graph

--- traumaDemo.py
+++ traumaDemo.py
@@ -10,42 +10,54 @@ from pyAudioAnalysis import audioFeatureExtraction
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as style
 
-FILE = "PTSD_female2.wav"
+#Define Global Variables
 
-def findFormants():
+FILE = ["PTSD_female.wav", "PTSD_female2.wav"]
+length = len(FILE)-1
+i = 0
+
+def findFormants(FILE, i, length):
 
     #Define Variables
     nfft = 256
     X = 32
     nceps = 64
 
-    #Import & Analyze Audio Signal
-    [Fs, x] = audioBasicIO.readAudioFile(FILE);
+    
+    
+    #for i in FILE:
+    while i <= length:
+        #Import & Analyze Audio Signal
+        [Fs, x] = audioBasicIO.readAudioFile(FILE[i]);
+    
+        #Calculate Formant Frequencies
+        frqs = audioFeatureExtraction.phormants(x[:,0], Fs);
+        print frqs
+        
+        #Visualize Data
+        plt.title('MFCC Analysis')
+        #plt.subplot(1,1,1); plt.plot(F[9,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC');
+        #plt.subplot(1,1,1); plt.plot(ceps[:,0]); plt.xlabel('Frame no'); plt.ylabel('Freq (Hz)');
+        plt.subplot(1,1,1); plt.plot(frqs); plt.xlabel('Frame no'); plt.ylabel('Freq (Hz)');
+        
+        i += 1
 
     #[HR, f0] = audioFeatureExtraction.stHarmonic(x[:,0], Fs)
-
-    #Calculate Formant Frequencies
-    frqs = audioFeatureExtraction.phormants(x[:,0], Fs);
 
     #Calculate MFCCs
     [fbank, freqs] = audioFeatureExtraction.mfccInitFilterBanks(Fs, nfft);
     ceps = audioFeatureExtraction.stMFCC(X, fbank, nceps);
 
-    return frqs
+    return frqs, x
 
 
 #Call Functions
-frqs = findFormants();
-print frqs
-
-
-#Visualize Data
-plt.title('MFCC Analysis')
-#plt.subplot(1,1,1); plt.plot(F[9,:]); plt.xlabel('Frame no'); plt.ylabel('MFCC');
-#plt.subplot(1,1,1); plt.plot(ceps[:,0]); plt.xlabel('Frame no'); plt.ylabel('Freq (Hz)');
-plt.subplot(1,1,1); plt.plot(frqs); plt.xlabel('Frame no'); plt.ylabel('Freq (Hz)');
-
+[frqs, x] = findFormants(FILE, i, length);
+#print frqs
 
 plt.show()
+
+
+
 
 
