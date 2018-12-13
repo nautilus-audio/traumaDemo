e0070dc4f71fe69ef5cff9661fca0d3cad438c5c
Kamilah Mitchell <kamilahamitchell@gmail.com>
2018-12-13 12:57:08 -0600

    Update traumaDemo.py

--- src/traumaDemo.py
+++ src/traumaDemo.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 '''
+Project Goals: 
+Use voice analysis to calculate Formant Frequencies of Voice Samples of PTSD, Anxiety and Depression patients (as well as a control set) to compare data on these trauma-related disorders. Implement in Python program.
 
-Next Steps:
-- optimize
+Premise: 
+Anxiety and depression are more common mental disorders, and the diagnosis of these can cause PTSD to be overlooked. It is very common for PTSD patients to experience anxiety and depression as well. Early detection can prevent the compilation of diseases due to the long-term effects of living without proper treatment. A formant is a concentration of acoustic energy around a particular frequency, or a resonance in the vocal tract. Formants occur at roughly 1000Hz intervals.
+
+Hypothesis: 
+Depression samples will have lower frequencies, PTSD samples will have the highest. This is based off of past research suggesting that formant frequencies of schizophrenia patients are lower during negative symptoms (apathy, lack of emotion, poor or non-existent social functioning) and higher during positive symptoms (hallucinations, delusions, racing thoughts).
 '''
 
 from pyAudioAnalysis import audioBasicIO
